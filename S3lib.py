338890c0aee9f7d723c438a8f0fee97528b085f3
Henry Dykhne <edykhne@uoguelph.ca>
2021-09-24 01:50:08 -0400

    changed error message structure and printing. added folder creation

--- S3lib.py
+++ S3lib.py
@@ -1,3 +1,4 @@
+import copy
 import boto3
 from botocore.exceptions import ClientError
 import configparser
@@ -5,12 +6,16 @@ import re
 import logging
 
 class S3:
+    initial_response = {"code": 1, "text":"unspecified error"}
+
     def __init__(self):
         self.client = None
         self.config()
         self.commands = {}
         self.commands['lc_copy'] = self.lc_copy
         self.commands['cl_copy'] = self.cl_copy
+        self.commands['create_bucket'] = self.create_bucket
+        self.commands['create_folder'] = self.create_folder
         self.s3path = ""
 
     def config(self):
@@ -26,14 +31,15 @@ class S3:
             aws_access_key_id = ACCESS_KEY,
             aws_secret_access_key = SECRET_KEY,
             region_name = REGION
-
-            #aws_session_token=SESSION_TOKEN
         )
         self.client = client
 
     def lc_copy(self, args):
+        response = copy.deepcopy(self.initial_response)
+
         if len(args) != 3:
-            return "invalid number of arguments"
+            response["text"] = "invalid number of arguments"
+            return response
 
         destinationSplit = args[2].split(':')
         if len(destinationSplit) != 2:
@@ -41,21 +47,26 @@ class S3:
             #make sure to manually add : after buckets in path during ch_folder
         
         if len(destinationSplit) != 2:
-            return "invalid destination format"
+            response["text"] = "invalid destination format"
+            return response
 
         bucketName = destinationSplit[0]
         objName = destinationSplit[1]
         try:
             self.client.upload_file(args[1], bucketName, objName)
-            response = ""
+            response["code"] = 0
+            response["text"] = ""
         except (ClientError, FileNotFoundError) as e:
-            logging.error(e)
-            response = e
+            response["text"] = e
+
         return response
 
     def cl_copy(self, args):
+        response = copy.deepcopy(self.initial_response)
+
         if len(args) != 3:
-            return "invalid number of arguments"
+            response["text"] = "invalid number of arguments"
+            return response
 
         sourceSplit = args[1].split(':')
         if len(sourceSplit) != 2:
@@ -63,14 +74,84 @@ class S3:
             #make sure to manually add : after buckets in path during ch_folder
         
         if len(sourceSplit) != 2:
-            return "invalid source format"
+            response["text"] = "invalid source format"
+            return response
 
         bucketName = sourceSplit[0]
         objName = sourceSplit[1]
         try:
             self.client.download_file(bucketName, objName, args[2])
-            response = ""
+            response["code"] = 0
+            response["text"] = ""
         except (ClientError, FileNotFoundError) as e:
-            logging.error(e)
-            response = e
+            response["text"] = e
+
+        return response
+
+    def create_bucket(self, args):
+        response = copy.deepcopy(self.initial_response)
+
+        if len(args) != 2:
+            response["text"] = "wrong number of arguments"
+            return response
+        
+        if not (3 < len(args[1]) <= 63):
+            response["text"] = "invalid bucket name\nbucket names must be between 3 and 63 characters long"
+            return response
+
+        if not re.match("^[a-z0-9\.-]*$", args[1]):
+            response["text"] = "invalid bucket name\nbucket names can consist only of lowercase letters, numbers, dots (.), and hyphens (-)"
+            return response
+
+        if not args[1][0].isalnum() or not args[1][-1].isalnum():
+            response["text"] = "invalid bucket name\nbbucket names must begin and end with a letter or number"
+            return response
+
+        if re.match("^([0-9]+\.)+[0-9]+$", args[1]):
+            response["text"] = "invalid bucket name\nbucket names must not be formatted as an IP address (for example, 192.168.5.4)"
+            return response
+
+        if args[1].startswith("xn--"):
+            response["text"] = "invalid bucket name\nbucket names must not start with the prefix xn--"
+            return response
+
+        if args[1].endswith("-s3alias"):
+            response["text"] = "invalid bucket name\nbucket names must not end with the suffix"
+            return response
+
+        try:
+            location = {'LocationConstraint': self.client.meta.region_name}
+            self.client.create_bucket(Bucket=args[1], CreateBucketConfiguration=location)
+            response["code"] = 0
+            response["text"] = ""
+        except ClientError as e:
+            response["text"] = e
+
+        return response
+    
+    def create_folder(self, args):
+        response = copy.deepcopy(self.initial_response)
+
+        if len(args) != 2:
+            response["text"] = "invalid number of arguments"
+            return response
+
+        destinationSplit = args[1].split(':')
+        if len(destinationSplit) != 2:
+            destinationSplit = (self.path + args[2]).split(':') 
+            #make sure to manually add : after buckets in path during ch_folder
+        
+        if len(destinationSplit) != 2:
+            response["text"] = "invalid destination format"
+            return response
+
+        bucketName = destinationSplit[0]
+        folderName = destinationSplit[1] + "/"
+        try:
+            self.client.put_object(Bucket = bucketName, Key = folderName)
+            response["code"] = 0
+            response["text"] = ""
+        except (ClientError, FileNotFoundError) as e:
+            response["text"] = e
+        
         return response
--- S5.py
+++ S5.py
@@ -3,9 +3,25 @@ import shlex
 import readline
 from S3lib import S3
 
+path = os.getcwd()
+
+def cd(args):
+    if len(args) == 1 or args[1] == "" or args[1] == " ":
+        args[1] = ""
+        
+    try:
+        os.chdir(args[1])
+    except:
+        return "invalid path"
+
+    path = os.getcwd()
+    return ''
+
 #commands
-shell_specific_commands = {}
+shell_commands = {}
+shell_commands['cd'] = cd
 s3 = S3()
+
 
 
 if s3.client is not None: #this does not work. fix it
@@ -17,18 +33,19 @@ else:
     print("Please review procedures for authenticating your account on AWS S3")
 
 while True:
-    inputText = input("S5> ")
+    inputText = input(path + ":S5> ")
     args = shlex.split(inputText)
-
-    print(args)
 
     if len(args) == 0:
         continue
     elif args[0] == "exit" or args[0] == "quit":
         exit()
     elif args[0] in s3.commands.keys():
-        print(s3.commands[args[0]](args))
-    elif args[0] in shell_specific_commands.keys():
-        shell_specific_commands[args[0]](args)
+        response = s3.commands[args[0]](args)
+    elif args[0] in shell_commands.keys():
+        response = shell_commands[args[0]](args)
     else:
         os.system(inputText)
+
+    if response["code"] != 0:
+        print(response["text"])
